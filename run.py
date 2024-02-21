217945ac3cddc5f2f5c3ccaaf77e36aebbfb6c35
oliversulyok <sulyokoliver@msn.com>
2024-02-21 21:20:17 +0100

    Only discord integration

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ from get_docker_secret import get_docker_secret
 
 
 def main():
-    import integrations.discord
     discord_token = get_docker_secret('discord-token', default='None')
-    integrations.discord.init(discord_token)
+    import classes.integrations.discord as DiscordIntegration
+    DiscordIntegration.init(discord_token)
 
 if __name__ == "__main__":
     main()
